f293c7def7e613187edb6434cb910e8ef9e09684
Worldlyth <103929543+Worldlyth@users.noreply.github.com>
2022-06-20 18:55:35 +0300

    separate validation

--- changeit/src/components/Checklist/Form/Form.jsx
+++ changeit/src/components/Checklist/Form/Form.jsx
@@ -3,116 +3,40 @@ import { useState } from "react"
 import { FormInput } from "./FormItems/FormInput/FormInput"
 import { FormSelect } from "./FormItems/FormSelect/FormSelect"
 export const Form = () => {
-  const [field, setField] = useState({
-    firstName: {
-      value: "",
-      style: "item__field",
-      error: "",
-    },
-    // lastName: "",
-    // email: "",
-    // lifeEvent: "",
-    // lifeEventDate: "",
-  })
-
-  function validate(e) {  
-    const regCapitalized = /^[A-Z\s]*$/
-    const regName = /^[a-zA-Z\s]*$/
-    const fieldValue = e.target.value
-
-    if (!regName.test(fieldValue)) {
-      setError(" contained only letters")
-    } else if (!regCapitalized.test(fieldValue.charAt(0))) {
-      setError("capitalized")
-    } else if (!fieldValue) {
-      setError("filled")
-    } else if (fieldValue.length < 3) {
-      setError(" more than 3 letters")
-    } else {
-      setCorrect()
-    }
-  }
-
-  const setError = (message) => {
-    setField((prev) => ({
-      ...prev,
-      firstName: { ...prev.firstName, style: "field-error", error: message },
-    }))
-  }
-
-  const setCorrect = () => {
-    setField((prev) => ({
-      ...prev,
-      firstName: { ...prev.firstName, style: "field-correct", error: '' },
-    }))
-  }
-
-  const handleFirstName = (e) => {
-    setField((prev) => ({
-      ...prev,
-      firstName: { ...prev.firstName, value: e.target.value },
-    }))
-    validate(e)
-    console.log(e.target.value);
-  }
-
-  const reset = () => {
-    setField((prev) => ({
-      ...prev,
-      firstName: { ...prev.firstName, style: "item__field", error: "" },
-    }))
-  }
 
   return (
-    <form className="checklist__form bg_white" onSubmit={validate}>
+    <form className="checklist__form bg_white">
       <FormInput
-        className={field.firstName.style}
         type="text"
         placeholder="Enter your First Name"
         id="firstName"
         label="First Name"
-        onChange={handleFirstName}
-        value={field.firstName.value}
-        onBlur={validate}
-        onFocus={reset}
-        error={field.firstName.error}
       />
 
       <FormInput
-        className="item__field"
         type="text"
         placeholder="Enter your Last Name"
         id="lastName"
         label="Last Name"
-        onChange={(e) => setField({ ...field, lastName: e.target.value })}
-        value={field.lastName}
       />
 
       <FormInput
-        className="item__field"
         type="text"
         placeholder="Enter your Email"
         id="email"
         label="Email"
-        onChange={(e) => setField({ ...field, email: e.target.value })}
-        value={field.email}
       />
 
       <FormSelect
         label="Life Event"
         id="lifeEvent"
-        onChange={(e) => setField({ ...field, lifeEvent: e.target.value })}
-        value={field.lifeEvent}
       />
 
       <FormInput
-        className="item__field"
         type="text"
         placeholder="DD.MM.YYYY"
         id="lifeEventDate"
         label="Life Event Date"
-        onChange={(e) => setField({ ...field, lifeEventDate: e.target.value })}
-        value={field.lifeEventDate}
       />
 
       <div className="form__notice">
--- changeit/src/components/Checklist/Form/FormItems/FormInput/FormInput.jsx
+++ changeit/src/components/Checklist/Form/FormItems/FormInput/FormInput.jsx
@@ -1,32 +1,56 @@
 import React from "react"
 import "../FormInput/formInput.css"
+import { useState } from "react"
 
-export const FormInput = ({
-  id,
-  label,
-  type,
-  placeholder,
-  value,
-  onChange,
-  className,
-  onBlur,
-  onFocus,
-  error
-}) => {
+export const FormInput = ({ id, label, type, placeholder }) => {
+  const [field, setField] = useState({
+    value: "",
+    style: "item__field",
+    error: "",
+  })
 
+  let errorMessage
 
-  let errorMessage;
+  function validate(e) {
+    const regCapitalized = /^[A-Z\s]*$/
+    const regName = /^[a-zA-Z\s]*$/
+    const fieldValue = e.target.value
 
-    if (error !== "") {
-      errorMessage = `*The field must be ${error}`
+    if (!regName.test(fieldValue)) {
+      setError(" contained only letters")
+    } else if (!regCapitalized.test(fieldValue.charAt(0))) {
+      setError("capitalized")
+    } else if (!fieldValue) {
+      setError("filled")
+    } else if (fieldValue.length < 3) {
+      setError(" more than 3 letters")
+    } else {
+      setCorrect()
     }
+  }
 
-    const [field, setField] = useState ({
-      value: '',
-      style: 'item__field',
-      error: ''
-    })
-  
+  const setError = (message) => {
+    setField(field.error = message, field.style = 'field-error')
+    errorMessage = `*${label} must be ${field.error}`
+  }
+
+  const setCorrect = () => {
+    setField((prev) => ({
+      ...prev,
+      firstName: { ...prev.firstName, style: "field-correct", error: "" },
+    }))
+  }
+
+  const fieldHandler = (e) => {
+    setField((prev)=> ({...prev, value: e.target.value}))
+  }
+
+  const reset = () => {
+    setField((prev) => ({
+      ...prev,
+      firstName: { ...prev.firstName, style: "item__field", error: "" },
+    }))
+  }
 
   return (
     <div className="form__item">
@@ -34,17 +58,17 @@ export const FormInput = ({
         {label}
       </label>
       <input
-        className={className}
+        className={field.style}
         type={type}
         placeholder={placeholder}
         id={id}
         name={id}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-        onFocus={onFocus}
-      /> 
-    <div className="error-text">{errorMessage}</div>
+        onChange={fieldHandler}
+        value= {field.value}
+        onBlur ={validate}
+        onFocus = {reset}
+      />
+      <div className="error-text">{errorMessage}</div>
     </div>
   )
 }
